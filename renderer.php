716666f5ec9bdefa071f323a41b55cca090e2ebb
ysrivast2000 <55416450+ysrivast2000@users.noreply.github.com>
2021-04-06 13:19:58 -0600

    Update renderer.php

--- renderer.php
+++ renderer.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Similaririty Calcualtor question renderer class.
+ * Short Answer Similarity question renderer class.
  *
  * @package    qtype
  * @subpackage shortanssimilarity
